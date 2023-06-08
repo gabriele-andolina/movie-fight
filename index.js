8d7bff5189689dd9976affdecda97ca125e29f0c
gabriele-andolina <gabe.andolina@gmail.com>
2023-06-08 19:20:06 +0200

    Created fetchData() to access OMDb API

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hi there!');
+const fetchData = async () => {
+    const response = await fetch(
+        "http://www.omdbapi.com/?apikey=1d75e9fb&s=avengers"
+    )
+        .then((res) => res.json())
+        .then((data) => console.log(data));
+
+    return response;
+};
+
+fetchData();
