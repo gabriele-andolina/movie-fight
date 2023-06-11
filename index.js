78815f03128e0258142de305148200a00f4dcee6
gabriele-andolina <gabe.andolina@gmail.com>
2023-06-11 18:24:33 +0200

    Created div to show movie search results

--- index.js
+++ index.js
@@ -3,14 +3,28 @@ const fetchData = async (searchTerm) => {
         `http://www.omdbapi.com/?apikey=1d75e9fb&s=${searchTerm}`
     )
         .then((res) => res.json())
-        .then((data) => console.log(data));
+        .then((data) => {
+            return data.Search;
+        });
 
+    console.log(response);
     return response;
 };
 
 const input = document.querySelector("input");
 
-const onInput = (event) => {
-    fetchData(event.target.value);
+const onInput = async (event) => {
+    const movies = await fetchData(event.target.value);
+
+    for (let movie of movies) {
+        const div = document.createElement("div");
+
+        div.innerHTML = `
+            <img src="${movie.Poster}" />
+            <h1>${movie.Title}</h1>
+        `;
+
+        document.querySelector("#target").appendChild(div);
+    }
 };
 input.addEventListener("input", debounce(onInput, 500));
